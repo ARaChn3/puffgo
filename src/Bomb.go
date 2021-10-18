32873210b54d191bb87cccc2297057e6e1f76601
NovusEdge <novusedge0@gmail.com>
2021-10-17 17:42:52 -0700

    Redefined type: Bomb to type: LogicBomb and redefined the structure of the type and it's functions

--- src/Bomb.go
+++ src/Bomb.go
@@ -1,83 +1,43 @@
 package puffgo
 
 import (
-	"bytes"
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
-	"log"
-	"os/exec"
-	"time"
 )
 
-/*
-Bomb: ...
-Fields:
-	ID [string]
-		A random hex string used to identify the bomb
+//LogicBomb: type implementing a logic-bomb for UNIX/Linux based systems.
+type LogicBomb struct {
 
-	Exec [string]
-		A string of commands to execute when the bomb goes off
+	// Listner represents an event-listner defined in this library.
+	Listner *EventListner
 
-	RunOnBoot [bool]
-		-_-
+	// ExecutionFunction specifies the function
+	// which will be executed when the bomb is triggered and it goes off
+	ExecutionFunction func(...interface{})
 
-	Timer [*time.Timer]
-		Timer for the bomb
+	// ExecutionFunctionArgs specifies the arguments to be passed into ExecutionFunction
+	ExecutionFunctionArgs interface{}
 
-
-*/
-type Bomb struct {
-	Deadline *time.Time
-	Exec     string
-	ID       string
+	// BombId represents a random hex string used to identify the bomb.
+	BombID string
 }
 
-func NewBomb(deadline *time.Time, e string) Bomb {
-	id, _ := randomHex(10)
+// NewBomb returns an instance of LogicBomb, which can be
+// implanted by Implant(), to be triggered when conditions
+// for it to do so are met.
+func NewBomb(listner Listner, execFunc func(...interface{}), execFuncArgs interface{}) (lb *LogicBomb) {
+	lb.Listner = listner
+	lb.BombID, _ = randomHex(10)
+	lb.ExecutionFunction = execFunc
+	lb.ExecutionFunctionArgs = execFuncArgs
 
-	return Bomb{
-		ID:       id,
-		Deadline: deadline,
-		Exec:     e,
-	}
+	return
 }
 
-//Implant: Implants a logic bomb.
-func (b *Bomb) Implant() {
-	var stdout, stderr bytes.Buffer
-	f := fmt.Sprintf("usr/sbin/LB%s_%s.service", b.ID, b.Deadline)
-	cmd := exec.Command("touch", []string{f}...)
-
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil || stderr.String() != "" {
-		log.Fatal(fmt.Sprintf("%s[!] E: Error while implanting bomb.%s", ColorRed, ColorReset))
-		log.Printf("%s[*] Log: %s\nError: %s\n\nStderr: %s\n", ColorYellow, ColorReset, err, stderr.String())
-	}
-
-	log.Printf("%sStdout: %s%s\n", ColorCyan, stdout.String(), ColorReset)
-}
-
-//Remove: removes previously implanted bomb
-func (b *Bomb) Remove() {
-	f := fmt.Sprintf("usr/sbin/LB%s_%s.service", b.ID, b.Deadline)
-
-	err := exec.Command("rm", []string{f}...).Run()
-	if err != nil {
-		log.Fatal(fmt.Sprintf("%s[!]E: %s%s\n", ColorRed, err, ColorReset))
-	}
-}
-
-//randomHex generates a randomized hex string of length "n"
 func randomHex(n int) (string, error) {
 	bytes := make([]byte, n)
-
 	if _, err := rand.Read(bytes); err != nil {
 		return "", err
 	}
-
 	return hex.EncodeToString(bytes), nil
 }
