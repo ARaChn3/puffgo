10da5a63344e03df7192e8af852b4bf4b982fce3
NovusEdge <novusedge0@gmail.com>
2021-10-18 13:16:16 -0700

    Completed the implementation of LogicBomb

--- src/Bomb.go
+++ src/Bomb.go
@@ -3,37 +3,71 @@ package puffgo
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"sync"
 )
 
 //LogicBomb: type implementing a logic-bomb for UNIX/Linux based systems.
 type LogicBomb struct {
 
-	// Listner represents an event-listner defined in this library.
-	Listner *EventListner
+	// Listener represents an event-listner defined in this library.
+	Listener *EventListener
 
 	// ExecutionFunction specifies the function
 	// which will be executed when the bomb is triggered and it goes off
-	ExecutionFunction func(...interface{})
+	ExecutionFunction func()
 
-	// ExecutionFunctionArgs specifies the arguments to be passed into ExecutionFunction
-	ExecutionFunctionArgs interface{}
-
-	// BombId represents a random hex string used to identify the bomb.
+	// BombId specifies a random hex string used to identify the bomb.
 	BombID string
+
+	// PID specifies the process-id of the persistent logic-bomb program running.
+	// It holds nil until the mainloop of the event-listner is started.
+	PID *int
 }
 
 // NewBomb returns an instance of LogicBomb, which can be
 // implanted by Implant(), to be triggered when conditions
 // for it to do so are met.
-func NewBomb(listner Listner, execFunc func(...interface{}), execFuncArgs interface{}) (lb *LogicBomb) {
-	lb.Listner = listner
+func NewBomb(listener EventListener, execFunc func()) *LogicBomb {
+	var lb LogicBomb
+
+	lb.Listener = &listener
 	lb.BombID, _ = randomHex(10)
 	lb.ExecutionFunction = execFunc
-	lb.ExecutionFunctionArgs = execFuncArgs
+	lb.PID = lb.Listener.PID
 
-	return
+	return &lb
 }
 
+func (lb *LogicBomb) Arm() {
+	var wg sync.WaitGroup
+
+	// Run listner's mainloop
+	go func() {
+		defer wg.Done()
+		lb.Listener.Mainloop()
+	}()
+
+	// Check for trigger...
+	go func() {
+		defer wg.Done()
+		for {
+			if isTriggered := <-lb.Listener.TriggerChannel; isTriggered {
+				lb.ExecutionFunction()
+				lb.Listener.Terminate()
+				break
+			}
+		}
+	}()
+
+	wg.Add(2)
+	wg.Wait()
+}
+
+func (lb *LogicBomb) Disarm() {
+	lb.Listener.Terminate()
+}
+
+// Used for generating the BombID on creation of LogicBombs
 func randomHex(n int) (string, error) {
 	bytes := make([]byte, n)
 	if _, err := rand.Read(bytes); err != nil {
